e40b8061e9802ad59f590dee2380a2cdb491bbf0
Be9st <37420684+Be9st@users.noreply.github.com>
2019-07-01 03:32:23 +0300

    Рандомно посылает нахуй всех сразу

--- app/blog/blog_app/views.py
+++ app/blog/blog_app/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from random import choice
+from random import choice, shuffle
 # Create your views here.
 
 
 def post_list(requset):
     n = ['Завер', 'Павел', 'Олег', 'Артём', 'Серёга']
-    return render(requset, 'blog_app/index.html', context={'names': n})
+    shuffle(n)
+    random_name = choice(n)
+    return render(requset, 'blog_app/index.html', context={'names': n, 'random_name':random_name})
