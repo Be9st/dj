3c5ba7f4c7313fb59ba338ba62e18109e6532215
Be9st <37420684+Be9st@users.noreply.github.com>
2019-07-01 23:02:33 +0300

    Посылает нахуй в формате ЛГБТ

--- app/blog/blog_app/views.py
+++ app/blog/blog_app/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from random import choice, shuffle
-# Create your views here.
-
 
 def post_list(requset):
-    n = ['Завер', 'Павел', 'Олег', 'Артём', 'Серёга']
-    shuffle(n)
-    random_name = choice(n)
-    return render(requset, 'blog_app/index.html', context={'names': n, 'random_name':random_name})
+    names = ['Завер', 'Павел', 'Олег', 'Артём', 'Серёга']
+    colors = [
+        'text-primary', 
+        'text-info', 
+        'text-warning', 
+        'text-danger',
+        'text-success']
+    shuffle(colors) 
+    shuffle(names)
+    values = dict(zip(colors, names))
+    random_name = choice(names)
+    return render(requset, 'blog_app/index.html', 
+        context={
+        'random_name': random_name,
+        'values': values
+        #'names': names,
+        #'color': color
+        })
